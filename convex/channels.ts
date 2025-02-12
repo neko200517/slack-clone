3cdccc6e6d9ee231a440552e860661a192c87e16
Jun Terayama <pam130901@gmail.com>
2025-02-12 23:02:23 +0900

    Channel header

--- convex/channels.ts
+++ convex/channels.ts
@@ -37,6 +37,118 @@ export const create = mutation({
   },
 });
 
+export const update = mutation({
+  args: {
+    id: v.id('channels'),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    // ユーザー確認
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error('Unauthorized');
+    }
+
+    // チャンネル確認
+    const channel = await ctx.db.get(args.id);
+
+    if (!channel) {
+      throw new Error('Channel not found');
+    }
+
+    const workspaceId = channel.workspaceId;
+
+    // メンバー確認
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', workspaceId).eq('userId', userId)
+      )
+      .unique();
+
+    if (!member || member.role !== 'admin') {
+      throw new Error('Unauthrized');
+    }
+
+    await ctx.db.patch(args.id, { name: args.name });
+
+    return args.id;
+  },
+});
+
+export const remove = mutation({
+  args: {
+    id: v.id('channels'),
+  },
+  handler: async (ctx, args) => {
+    // ユーザー確認
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error('Unauthorized');
+    }
+
+    // チャンネル確認
+    const channel = await ctx.db.get(args.id);
+
+    if (!channel) {
+      throw new Error('Channel not found');
+    }
+
+    const workspaceId = channel.workspaceId;
+
+    // メンバー確認
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', workspaceId).eq('userId', userId)
+      )
+      .unique();
+
+    if (!member || member.role !== 'admin') {
+      throw new Error('Unauthrized');
+    }
+
+    await ctx.db.delete(args.id);
+
+    return args.id;
+  },
+});
+
+export const getById = query({
+  args: { id: v.id('channels') },
+  handler: async (ctx, args) => {
+    // ユーザーの存在確認
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      return null;
+    }
+
+    // チャンネルの存在確認
+    const channel = await ctx.db.get(args.id);
+
+    if (!channel) {
+      return null;
+    }
+
+    // メンバーの存在確認
+    const member = await ctx.db
+      .query('members')
+      .withIndex('by_workspace_id_user_id', (q) =>
+        q.eq('workspaceId', channel.workspaceId).eq('userId', userId)
+      )
+      .unique();
+
+    if (!member) {
+      return null;
+    }
+
+    return channel;
+  },
+});
+
 export const get = query({
   args: { workspaceId: v.id('workspaces') },
   handler: async (ctx, args) => {
